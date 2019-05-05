5649b6ef05f4b23cdd2439a5461c03187eaf8d90
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-05 00:33:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,22 @@
-# def line(katz_deli)
-#   if katz_deli == 0
-#   puts "The line is currently empty."
-# else
-#   current_line = "The line is currently:"
-  
-# end
-
-# def take_a_number(name)
-  
-  
-
 def line(katz_deli)
   if katz_deli.size == 0
   puts "The line is currently empty."
   else
-     result="The line is currently:"
+     result = "The line is currently:"
      katz_deli.each do |customer|
-       result += " #{katz_deli.index(customer)+1}. #{customer}"
+       result += "#{katz_deli.index(customer)+1}. #{customer}"
      end
      puts result
    end
 end
 
- def take_a_number(deli, name)
+def take_a_number(deli, name)
     deli << name
     puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
 
- def now_serving(deli)
+def now_serving(deli)
   if deli.empty?
     puts "There is nobody waiting to be served!"
   else
