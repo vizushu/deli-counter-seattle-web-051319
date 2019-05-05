a11ce11e2c51905641e008ed35d098bd66db4b35
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-05 00:29:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
-def line(katz_deli)
-  if katz_deli == 0
-  puts "The line is currently empty."
-else
-  current_line = "The line is currently:"
-end
+# def line(katz_deli)
+#   if katz_deli == 0
+#   puts "The line is currently empty."
+# else
+#   current_line = "The line is currently:"
+  
+# end
 
-def take_a_number(name)
+# def take_a_number(name)
   
   
 katz_deli = []
