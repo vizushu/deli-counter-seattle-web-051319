184de9f0487ff72e293e6162d3c7646729a43d2e
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-05 00:32:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,13 +10,13 @@
   
   
 
-def line(in_line)
-  if in_line.size == 0
+def line(katz_deli)
+  if katz_deli.size == 0
   puts "The line is currently empty."
   else
      result="The line is currently:"
-     in_line.each do |customer|
-       result += " #{in_line.index(customer)+1}. #{customer}"
+     katz_deli.each do |customer|
+       result += " #{katz_deli.index(customer)+1}. #{customer}"
      end
      puts result
    end
